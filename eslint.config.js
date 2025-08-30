95c52b1ff5c5b8be5431017d32f77894e2c44fc8
Mikdat DOĞRU <mikdat@yamystudio.com>
2025-08-30 02:05:53 +0100

    migrate

---
+++ eslint.config.js
@@ -0,0 +1,138 @@
+const eslintJs = require('@eslint/js')
+const prettierConfig = require('eslint-config-prettier')
+const prettierPlugin = require('eslint-plugin-prettier')
+
+module.exports = [
+  eslintJs.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        Promise: 'readonly',
+        async: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        test: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        setImmediate: 'readonly',
+        clearImmediate: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly'
+      }
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'callback-return': 'off',
+      camelcase: [
+        'warn',
+        {
+          properties: 'always'
+        }
+      ],
+      'comma-style': ['warn', 'last'],
+      curly: ['error'],
+      eqeqeq: ['error', 'always'],
+      'eol-last': ['warn'],
+      'no-undef': 'error',
+      'handle-callback-err': ['error'],
+      'arrow-body-style': ['off'],
+      indent: ['off'],
+      'linebreak-style': ['error', 'unix'],
+      'no-dupe-keys': ['error'],
+      'no-duplicate-case': ['error'],
+      'no-extra-semi': ['warn'],
+      'no-labels': ['error'],
+      'no-mixed-spaces-and-tabs': ['error'],
+      'no-redeclare': ['warn'],
+      'no-return-assign': ['error', 'always'],
+      'no-sequences': ['error'],
+      'no-trailing-spaces': ['warn'],
+      'no-unexpected-multiline': ['warn'],
+      'no-unreachable': ['warn'],
+      'no-magic-numbers': ['off'],
+      'max-params': ['off'],
+      'max-len': ['off'],
+      'max-nested-callbacks': ['off'],
+      'new-cap': ['off'],
+      'consistent-this': ['error', 'that'],
+      'no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^unused($|[A-Z].*$)'
+        }
+      ],
+      'no-use-before-define': [
+        'error',
+        {
+          functions: false
+        }
+      ],
+      'no-var': 'error',
+      'no-useless-catch': 'off',
+      'no-async-promise-executor': 'off',
+      'one-var': ['warn', 'never'],
+      'prefer-arrow-callback': [
+        'warn',
+        {
+          allowNamedFunctions: true
+        }
+      ],
+      quotes: [
+        'warn',
+        'single',
+        {
+          avoidEscape: false,
+          allowTemplateLiterals: true
+        }
+      ],
+      'semi-spacing': [
+        'warn',
+        {
+          before: false,
+          after: true
+        }
+      ],
+      'semi-style': ['warn', 'last'],
+      'space-before-function-paren': ['off'],
+      'prettier/prettier': 'error'
+    }
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        test: 'readonly'
+      }
+    }
+  }
+]
